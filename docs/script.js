55bbf0f2d2f7152223d2fb92a35724081a08cf3d
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-06-10 22:52:37 -0500

    Updated index.html

--- docs/script.js
+++ docs/script.js
@@ -2289,6 +2289,48 @@ const defaultGradients = {
       fontsTable.innerHTML = s;
     }
   }
+function loadChecking() {
+ let href = window.location.href;
+ if(!href.includes(atob("YWxvbnNvYWxpYWdhLmdpdGh1Yi5pbw=="))) return;
+ let link = atob("aHR0cHM6Ly9hbG9uc29hcGkuZGlzY2xvdWQuYXBwL2NoZWNraW5nP3NpdGU9PHNpdGU+JmtleT08a2V5Pg==")
+  .replace(/<site>/g,"mc-skin").replace(/<key>/g,"KEY-A");
+ let counter = document.getElementById("online-counter");
+ if(counter) {
+   $.ajax({
+     url: link,
+     type: "GET", /* or type:"GET" or type:"PUT" */
+     dataType: "json",
+     data: {
+     },
+     success: function (result) {
+        //console.log(`Total fails: ${counter.dataset.failed}`)
+        counter.dataset.failed = "0";
+        counter.style.display = "flex";
+        if(isNaN(result)) {
+         counter.textContent = `🟡 You shouldn't be reading this. Report it on https://alonsoaliaga.com/discord`;
+         counter.style.backgroundColor = "yellow";
+        }else{
+         //counter.textContent = `🟢 ${result} user${result==1?``:`s`} online using our Minecraft Profile Picture Generator!`;
+         counter.textContent = `🟢 ${result} online using our Minecraft Skin 3D Viewer!`;
+         counter.style.backgroundColor = "green";
+        }
+     },
+     error: function (e) {
+      //console.log(`Total fails: ${counter.dataset.failed}`)
+      if(counter.style.display != "none") {
+        let currentFails = +counter.dataset.failed;
+        if(currentFails >= 1){
+          counter.style.display = "none"
+        }else{
+          counter.textContent = `🔴 Check your internet connection!`;
+          counter.style.backgroundColor = "#7c0000";
+          counter.dataset.failed = `${currentFails + 1}`
+        }
+      }
+     }
+   });
+ }
+}
   let times = 0;
   function loadCounter() {
    let href = window.location.href;
@@ -2725,9 +2767,9 @@ const defaultGradients = {
     }
   }
   // Example: lock all buttons for 5 seconds on page load
-  window.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.render-card').forEach(btn => lockAnimations(btn, 5));
-  });
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.render-card').forEach(btn => lockAnimations(btn, 5));
+});
 loadPage();
 function loadPage() {
     let i = []
@@ -2770,6 +2812,12 @@ loadCapes();
 document.addEventListener("DOMContentLoaded", () => {
   loadCounter();
   checkSite(window);
+  setTimeout(()=>{
+    loadChecking();
+    setInterval(()=>{
+      loadChecking();
+    },10000)
+  },2500)
 });
 function processAds() {
   lockAnimationsWithMessage(adLockedAnimations,`Disable AdBlock to access this animation!`)
