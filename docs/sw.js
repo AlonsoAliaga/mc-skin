a88edf0f784dc1cc601b9a4d5260030135eda511
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-06-11 12:27:15 -0500

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -4,28 +4,28 @@ const BLOCKED_URL_REGEXS = [
 ]
 const ALLOWED_REFERRER_DOMAIN = 'alonsoaliaga.github.io';
 
-self.addEventListener('fetch', function(event) {
-    const requestUrl = event.request.url;
-    const referrerUrl = event.request.referrer; // Get the referrer URL
-    // Create a URL object from the referrer for easier domain parsing
-    let referrerDomain = '';
-    try {
-        if (referrerUrl && referrerUrl !== 'about:client') { // 'about:client' can be a referrer for initial requests
-            const urlObj = new URL(referrerUrl);
-            referrerDomain = urlObj.hostname;
-        }
-    } catch (e) {
-        console.error("Error parsing referrer URL:", e);
-    }
-    if(referrerDomain.includes(ALLOWED_REFERRER_DOMAIN)) {
-        event.respondWith(fetch(event.request));
-    }else{
-        if(BLOCKED_URL_REGEXS.some(BLOCKED_URL_REGEX => BLOCKED_URL_REGEX.test(requestUrl))) {
-            event.respondWith(
-                new Response(null, { status: 200, statusText: 'Blocked by CORS' })
-            );
-        }else{
-            event.respondWith(fetch(event.request));
-        }
-    }
-});
+// self.addEventListener('fetch', function(event) {
+//     const requestUrl = event.request.url;
+//     const referrerUrl = event.request.referrer; // Get the referrer URL
+//     // Create a URL object from the referrer for easier domain parsing
+//     let referrerDomain = '';
+//     try {
+//         if (referrerUrl && referrerUrl !== 'about:client') { // 'about:client' can be a referrer for initial requests
+//             const urlObj = new URL(referrerUrl);
+//             referrerDomain = urlObj.hostname;
+//         }
+//     } catch (e) {
+//         console.error("Error parsing referrer URL:", e);
+//     }
+//     if(referrerDomain.includes(ALLOWED_REFERRER_DOMAIN)) {
+//         event.respondWith(fetch(event.request));
+//     }else{
+//         if(BLOCKED_URL_REGEXS.some(BLOCKED_URL_REGEX => BLOCKED_URL_REGEX.test(requestUrl))) {
+//             event.respondWith(
+//                 new Response(null, { status: 200, statusText: 'Blocked by CORS' })
+//             );
+//         }else{
+//             event.respondWith(fetch(event.request));
+//         }
+//     }
+// });
